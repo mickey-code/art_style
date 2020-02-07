7c9910fbd757f410581886d82924aea24ec2fa82
michael <mikelkarije@gmail.com>
2020-02-07 17:31:01 +0300

    updated the config file with development option or production option

---
+++ config.py
@@ -0,0 +1,15 @@
+import os
+
+class Config:
+    pass
+
+class PodConfig(Config):
+    
+    pass
+class DevConfig(Config):
+    DEBUG = True
+    
+config_options = {
+    'development':DevConfig,
+    'production':ProdConfig
+}
